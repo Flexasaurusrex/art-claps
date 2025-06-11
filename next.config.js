039dc743933d0e8a6df0d43bf92f843c3a623422
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-06-11 13:28:03 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   images: {
     domains: ['imagedelivery.net', 'res.cloudinary.com', 'i.imgur.com'],
   },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
 }
 
 module.exports = nextConfig
